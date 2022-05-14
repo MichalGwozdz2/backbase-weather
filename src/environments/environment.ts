ea19162722e63497cc52f0f409fba1b389a0d23b
michalgwozdz <michalgwozdz@payability.com>
2022-05-14 12:31:20 +0200

    mg: test commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   apiKeys: {
-    openWeatherApiKey: null',
+    openWeatherApiKey: null,
   },
   restApi: {
     openWeatherMap: 'https://api.openweathermap.org',
