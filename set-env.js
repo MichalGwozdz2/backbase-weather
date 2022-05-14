eacabab664884e28ee671dfede0c368f0cf64df6
michalgwozdz <michalgwozdz@payability.com>
2022-05-14 14:26:33 +0200

    mg: fix

--- set-env.js
+++ set-env.js
@@ -4,15 +4,7 @@ const devEnvPath = './src/environments/environment.ts';
 const prodEnvPath = './src/environments/environment.prod.ts';
 let key;
 
-if (process.env.MY_API_KEY) {
-  console.log('DETECTED SECRET');
-  console.log(process.env.MY_API_KEY);
-  key = `'${process.env.MY_API_KEY}'`;
-}
-
 if (process.argv[2]) {
-  console.log('DETECTED API KEY');
-  console.log(process.argv[2]);
   key = `'${process.argv[2]}'`;
 }
 
